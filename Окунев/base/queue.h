b710da009e7d2bf7e24ff4f5f3a5aa4fb0875da1
Boris Okunev <hait.08@mail.ru>
2016-12-13 23:27:40 +0300

    add priority

--- Окунев/base/queue.h
+++ Окунев/base/queue.h
@@ -6,6 +6,7 @@ struct TNode
 {
     T Value;
     TNode* pNext;
+    unsigned int priotiry;
 };
 
 template <class T>
@@ -16,16 +17,24 @@ class TQueue
 public:
     TQueue();
     TQueue(T &node);
+//insert
     void InsFirst(T &node);
     void InsLast(T &node);
     void Insert(T &node, size_t pos);
+//delete
+    void DelFirst();
+    void DelLast();
     void Delete(size_t pos);
+    size_t GetCount();
+    T GetElement(size_t pos); 
+    void SetElement(T elem, size_t pos);
+    unsigned int GetPriority(size_t pos);
+    void IncPriority(size_t pos);
 };
 
 template <class T>
 TQueue<T>::TQueue() : pFirst(nullptr), count(0)
-{
-}
+{}
 
 template <class T>
 TQueue<T>::TQueue(T &node)
@@ -35,6 +44,8 @@ TQueue<T>::TQueue(T &node)
     pFirst->pNext = nullptr;
     count = 1;
 }
+
+//insert
 
 template <class T>
 void TQueue<T>::InsFirst(T &node)
@@ -83,5 +94,96 @@ void TQueue<T>::Insert(T &node, size_t pos)
     }
 }
 
+//delete
+
+template <class T>
+void TQueue<T>::Delete(size_t pos)
+{
+    if (pos > count - 1)
+        throw("Attempt to remove a non-existent element");
+    TNode<T>* p = pFirst;
+    for (int i = 0; i < pos - 1; i++)
+        p->pNext;
+    if (pos == 0)
+    {
+        p = p->pNext;
+        delete pFirst;
+        pFirst = p;
+    }
+    else if (pos == count - 1)
+    {
+        delete p->pNext;
+        p->pNext = nullptr;
+    }
+    else
+    {
+        TNode<T> tmp = p->pNext->pNext;
+        delete p->pNext;
+        p->pNext = tmp;
+    }
+    count--;
+}
+
+template <class T>
+void TQueue<T>::DelFirst()
+{
+    Delete(0);
+}
+
+template <class T>
+void TQueue<T>::DelLast()
+{
+    Delete(count - 1);
+}
+
+template <class T>
+size_t TQueue<T>::GetCount()
+{
+    return count;
+}
+
+template <class T>
+T TQueue<T>::GetElement(size_t pos)
+{
+    if (pos > count - 1)
+        throw("Attempt to get a non-existent element");
+    TNode<T>* p = pFirst;
+    for (int i = 0; i < pos; i++)
+        p = p->pNext;
+    return p->Value;
+}
+
+template <class T>
+void TQueue<T>::SetElement(T elem, size_t pos)
+{
+    if (pos > count - 1)
+        throw("Attempt to get a non-existent element");
+    TNode<T>* p = pFirst;
+    for (int i = 0; i < pos; i++)
+        p = p->pNext;
+    p->Value = elem;
+}
+
+template <class T>
+unsigned int TQueue<T>::GetPriority(size_t pos)
+{
+    if (pos > count - 1)
+        throw("Attempt to get a non-existent element");
+    TNode<T>* p = pFirst;
+    for (int i = 0; i < pos; i++)
+        p = p->pNext;
+    return p->priotiry;
+}
+
+template <class T>
+void TQueue<T>::IncPriority(size_t pos)
+{
+    if (pos > count - 1)
+        throw("Attempt to get a non-existent element");
+    TNode<T>* p = pFirst;
+    for (int i = 0; i < pos; i++)
+        p = p->pNext;
+    p->priotiry++;
+}
 
 #endif
