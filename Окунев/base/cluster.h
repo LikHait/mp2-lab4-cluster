d1df74f8d8e91306c8183c60e792f063581f6f9a
Boris Okunev <hait.08@mail.ru>
2016-12-19 17:59:02 +0300

    v 0.6

--- Окунев/base/cluster.h
+++ Окунев/base/cluster.h
@@ -3,11 +3,26 @@
 
 #include "queue.h"
 
+struct process
+{
+    unsigned int tacts; //число тактов 1-10
+    unsigned int ProcNeed; //число требуемых процессоров
+};
+
 class TCluster
 {
-
+    unsigned int processors; //процессоров в кластере
+    unsigned int ProcFree; //на данный момент свободно процессоров
+    unsigned int MaxQueue; //максимальный размер очереди процессов
+    int* TicksLeft; //осталось тиков выполнени€(дл€ каждого €дра)
+    unsigned int T; //врем€ работы эмул€ции
+    int* LastResult; //последний результат запуска эмул€ции
+    double alpha; //коэффициент по€влени€ новых задач
 public:
-
+    TCluster(double a, unsigned int pr, unsigned int t, unsigned int qu);
+    void NewTask(TQueue<process>& qu);
+    void Emulate();
+    void GetRes();
 };
 
 #endif
